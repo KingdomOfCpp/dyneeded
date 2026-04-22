7e6f167b5fdb3cc62af2c60611fce47cf0f096dc
KingdomOfCpp <271990218+KingdomOfCpp@users.noreply.github.com>
2026-04-22 14:13:49 +0200

    more bible passages

--- src/bible.hpp
+++ src/bible.hpp
@@ -8,7 +8,7 @@ namespace dyneeded
     static constexpr string_view kBiblePassages[] = {
         "Once, on being asked by the Pharisees when the kingdom of God would come, Jesus replied, "
         "“The coming of the kingdom of God is not something that can be observed, nor will people say, 'Here it is' "
-        "or 'There it is' because the kingdom of God is in your midst.”",
+        "or 'There it is' because the kingdom of God is in your midst.",
 
         "When you were slaves to sin, you were free from the control of righteousness. "
         "What benefit did you reap at that time from the things you are now ashamed of? "
@@ -20,5 +20,27 @@ namespace dyneeded
         "satisfied with their income. This too is meaningless."
         "As goods increase, so do those who consume them. "
         "And what benefit are they to the owners except to feast their eyes on them?",
+
+        "Temptation should not take hold of you, except what is human. For God is faithful, and he will not permit you "
+        "to be tempted beyond your ability. Instead, he will effect his Providence, even during temptation, so that you "
+        "may be able to bear it.",
+
+        "How long will you slumber, you lazy one? When will you rise up from your sleep?",
+
+        "But he continued, “You are from below; I am from above. You are of this world; I am not of this world. "
+        "I told you that you would die in your sins; if you do not believe that I am he, you will indeed die in your sins.",
+
+        "In the beginning was the Word, and the Word was with God, and the Word was God. "
+        "He was with God in the beginning. "
+        "Through him all things were made; without him nothing was made that has been made. "
+        "In him was life, and that life was the light of all mankind. The light shines in the darkness, "
+        "and the darkness has not overcome it.",
+
+        "I know your deeds, that you are neither cold nor hot. I wish you were either one or the other! "
+        "So, because you are lukewarm—neither hot nor cold—I am about to spit you out of my mouth. "
+        "You say, ‘I am rich; I have acquired wealth and do not need a thing.’ "
+        "But you do not realize that you are wretched, pitiful, poor, blind and naked. "
+        "I counsel you to buy from me gold refined in the fire, so you can become rich; and white clothes to wear, "
+        "so you can cover your shameful nakedness; and salve to put on your eyes, so you can see."
     };
 } // namespace dyneeded
